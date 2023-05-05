57c31a8ceb3976783adfa449df8e42384727420d
luc-nguyen-174 <louiskingvn@gmail.com>
2023-05-05 16:51:03 +0700

    change orders entity name

--- src/main/java/com/example/demo/repo/IOrderRepository.java
+++ src/main/java/com/example/demo/repo/IOrderRepository.java
@@ -1,9 +1,9 @@
 package com.example.demo.repo;
 
-import com.example.demo.model.Order;
+import com.example.demo.model.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IOrderRepository extends JpaRepository<Order, Long> {
+public interface IOrderRepository extends JpaRepository<Orders, Long> {
 }

51e811e8e4ef052baf89565149104d2523766d5e
luc-nguyen-174 <louiskingvn@gmail.com>
2023-05-06 17:42:11 +0700

    create cart-item, cart, category controller

--- src/main/java/com/example/demo/controller/CartItemsController.java
+++ src/main/java/com/example/demo/controller/CartItemsController.java
@@ -1,12 +1,62 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.model.CartItems;
+import com.example.demo.service.carts.ICartItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping("api/cart-item")
+@RequestMapping("/api/cart-item")
 public class CartItemsController {
+    @Autowired
+    private ICartItemService cartItemService;
 
+    @GetMapping(value = {"", "/"})
+    public ResponseEntity<Iterable<CartItems>> showAllCartItems() {
+        List<CartItems> carts = (List<CartItems>) cartItemService.findAll();
+        if (carts.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(carts, HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<CartItems> createCartItems(@RequestBody CartItems cartItems) {
+        return new ResponseEntity<>(cartItemService.save(cartItems), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<CartItems> updateCartsItems(@RequestBody CartItems cartItems, @PathVariable Long id) {
+        Optional<CartItems> cartItemsOptional = cartItemService.findOne(id);
+        if (cartItemsOptional.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        cartItems.setId(id);
+        return new ResponseEntity<>(cartItemService.save(cartItems), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<CartItems> deleteCartItems(@PathVariable Long id) {
+        Optional<CartItems> cartItemsOptional = cartItemService.findOne(id);
+        if (cartItemsOptional.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        cartItemService.remove(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CartItems> viewCartItems(@PathVariable Long id) {
+        Optional<CartItems> cartItems = cartItemService.findOne(id);
+        if (cartItems.isPresent()) {
+            return new ResponseEntity<>(cartItems.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
